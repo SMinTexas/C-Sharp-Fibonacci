6761d48f256296de31af0dd61e5252794609d846
Steven Murray <32107837+SMinTexas@users.noreply.github.com>
2019-03-26 19:58:25 -0500

    Add files via upload

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+
+        public static void Fibonacci(int n)
+        {
+            int a = 0, b = 1, c = 0;
+            Console.Write("{0} {1}", a, b);
+            for (int i = 2; i < n; i++)
+            {
+                c = a + b;
+                Console.Write(" {0}", c);
+                a = b;
+                b = c;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the nth number of the Fibonacci Series: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Fibonacci(number);
+
+            Console.Read();
+        }
+    }
+}
